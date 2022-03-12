5fc12c99ee48c39fe6f0030506ac058cf1c4e067
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-03-11 19:35:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 function setup(){
     canvas=createCanvas(550, 500);
-    canvas.position(350, 100);
+    canvas.position(560, 100);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+function modelLoaded(){
+    console.log('PoseNet Is Intialized!');
+}
+function draw(){
+    background("#092AAD")
+}
+
+
