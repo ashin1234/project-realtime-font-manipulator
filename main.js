7d1dbd09d050eeafb3c9782ab2e9db3b33abd3e4
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-03-18 20:44:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup(){
     canvas=createCanvas(550, 500);
     canvas.position(560, 100);
@@ -9,6 +14,13 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX +"rightWristX = " + rightWristX + "difference = " + difference);
     }
 }
 function modelLoaded(){
@@ -16,6 +28,12 @@ function modelLoaded(){
 }
 function draw(){
     background("#092AAD")
+    document.getElementById("square_side").innerHTML = "Text's size will be = " + difference + "px";
+    fill('#ff2d00');
+    stroke('#ff2d00');
+   textSize(difference);
+   text("Ashin",noseX,noseY);
+
 }
 
 
